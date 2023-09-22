b311fd88bb09f1715eff8473b431a131602b53fa
Craig Puxty <craig_puxty@talktalk.net>
2023-09-22 13:31:39 +0100

    Added utility function getPrevNextPages & Footer for pagination. Updated Gallery & results/[...myParams]/page.

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,28 +1,52 @@
-import fetchImages from '@/lib/fetchImages';
 import type { ImagesResults } from '@/models/Images';
+
+// Components
 import ImgConatiner from './ImgConatiner';
+import Footer from './Footer';
+
+// Utilities
+import fetchImages from '@/lib/fetchImages';
 import addBlurredDataUrls from '@/lib/getBase64';
+import getPrevNextPages from '@/lib/getPrevNextPages';
 
 type Props = {
   term?: string | undefined;
+  page?: string | undefined;
 };
 
-export default async function Gallery({ term }: Props) {
-  const url = term
-    ? `https://api.pexels.com/v1/search?query=${term}`
-    : 'https://api.pexels.com/v1/curated';
+export default async function Gallery({ term = 'curated', page }: Props) {
+  let url;
+  if (term === 'curated' && page) {
+    url = `https://api.pexels.com/v1/curated?page=${page}`;
+  } else if (term === 'curated') {
+    url = 'https://api.pexels.com/v1/curated';
+  } else if (!page) {
+    url = `https://api.pexels.com/v1/search?query=${term}`;
+  } else {
+    url = `https://api.pexels.com/v1/search?query=${term}&page=${page}`;
+  }
+
   const images: ImagesResults | undefined = await fetchImages(url);
 
-  if (!images) {
+  if (!images || images.per_page === 0) {
     return <h2 className='m-4 text-2xl font-bold'>No Images Found</h2>;
   }
   const photosWithBlur = await addBlurredDataUrls(images);
 
+  // Calculate pagination
+
+  const { prevPage, nextPage } = getPrevNextPages(images);
+
+  const footerProps = { term, page, nextPage, prevPage };
+
   return (
-    <section className='px-1 my-3 grid grid-cols-gallery auto-rows-[10px]'>
-      {photosWithBlur.map((photo) => (
-        <ImgConatiner key={photo.id} photo={photo} />
-      ))}
-    </section>
+    <>
+      <section className='px-1 my-3 grid grid-cols-gallery auto-rows-[10px]'>
+        {photosWithBlur.map((photo) => (
+          <ImgConatiner key={photo.id} photo={photo} />
+        ))}
+      </section>
+      <Footer {...footerProps} />
+    </>
   );
 }
